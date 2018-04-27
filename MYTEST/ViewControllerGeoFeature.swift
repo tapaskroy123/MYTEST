b0264691c3fe242e684e073fe02382d41d2a4939
Tapas Roy <tapasr@usta405132-mac>
2018-04-26 22:12:08 -0400

    added Geo Location

---
+++ MYTEST/ViewControllerGeoFeature.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerGeoFeature.swift
+//  MYTEST
+//
+//  Created by Roy, Tapas on 4/26/18.
+//  Copyright © 2018 Roy, Tapas. All rights reserved.
+//
+
+import Foundation
+class GeoFeature{
+    func GeoLocationGet()->String{
+        return "Location"
+    }
+}
