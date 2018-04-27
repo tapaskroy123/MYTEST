f66f2a2f60b75e3ffd6280b6e99b8725656e2b2b
Tapas Roy <tapasr@usta405132-mac>
2018-04-26 22:05:55 -0400

    added map feature

--- MYTEST/ViewController.swift
+++ MYTEST/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello World")
+        print("Added MapFEature")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
